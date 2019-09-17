87391c5cf370544192acc37022ed79012b1bdb02
AlanaZupp <laniree@live.com>
2019-09-18 10:01:05 +1200

    form further fleshed out

--- src/components/searchpage.component.js
+++ src/components/searchpage.component.js
@@ -3,7 +3,10 @@ import "./searchpage.css";
 
 //array of json tree objects
 import {trees} from "./treeSelect.js";
+var results;
 
+//class that creates and holds the form for filtering between tree types upon search
+//will return list of tree objects that meet the users criteria.
 export default class SearchPage extends Component{
     render(){
         return(
@@ -11,14 +14,17 @@ export default class SearchPage extends Component{
                 <div className="searchBar">
                     <p>Find the tree(s) you're looking for...</p>
                     <div className="search">
-                        <input type="text" placeholder="Search.."></input>
+                        <input type="text" placeholder="Search.." id="searchBox"></input>
+                        <button type="button" onClick={this.searchRes}>Search</button><br/><br/>
                     </div>
                 </div>
                 <div className="options">
                     <p>Category</p><br/>
                     <form id="categoryF">
+                        <label>Unchecked</label>
+                        <input type="radio" name="category" value="none" defaultChecked/>
                         <label>Fruit Tree</label>
-                        <input type="radio" name="category" value="fruit" defaultChecked/>
+                        <input type="radio" name="category" value="fruit" />
                         <label>Hedge</label>
                         <input type="radio" name="category" value="hedge"/>
                         <label>Evergreen</label>
@@ -35,8 +41,10 @@ export default class SearchPage extends Component{
                     <p>Conditions</p><br/><br/>
                     <p className="subtitle">Soil Drainage</p>
                     <form id="soilConF">
+                        <label>Unchecked</label>
+                        <input type="radio" name="soilCon" value="none" defaultChecked/>
                         <label>Fast</label>
-                        <input type="radio" name="soilCon" value="fast" defaultChecked/>
+                        <input type="radio" name="soilCon" value="fast"/>
                         <label>Med</label>
                         <input type="radio" name="soilCon" value="medDrain"/>
                         <label>Slow</label>
@@ -46,8 +54,10 @@ export default class SearchPage extends Component{
                     </form>
                     <br/><p className="subtitle">Sunlight</p>
                     <form id="sunConF">
+                        <label>Unchecked</label>
+                        <input type="radio" name="sunCon" value="none" defaultChecked/>
                         <label>Sunny</label>
-                        <input type="radio" name="sunCon" value="sunny" defaultChecked/>
+                        <input type="radio" name="sunCon" value="sunny"/>
                         <label>Medium</label>
                         <input type="radio" name="sunCon" value="medSun"/>
                         <label>Shady</label>
@@ -57,36 +67,82 @@ export default class SearchPage extends Component{
                     </form><br/>
                     <p>Maintenance</p><br/>
                     <form id="maintenF">
+                        <label>Unchecked</label>
+                        <input type="radio" name="mainten" value="none" defaultChecked/>
                         <label>High</label>
-                        <input type="radio"  name="mainten" value="highMain" defaultChecked/>
+                        <input type="radio"  name="mainten" value="highMain"/>
                         <label>Medium</label>
                         <input type="radio"  name="mainten" value="medMain"/>
                         <label>Low</label>
                         <input type="radio"  name="mainten" value="lowMain"/>
                     </form>
-                    <p>Max Tree Height</p><br/><br/>
+                    <p>Max Tree Height</p><br/>
+                    <form id="heightF">
+                        <label>Unchecked</label>
+                        <input type="radio" name="heightV" value="none" defaultChecked/>
+                        <label>Less than 1M</label>
+                        <input type="radio"  name="heightV" value="<1"/>
+                        <label>1-2M</label>
+                        <input type="radio"  name="heightV" value="1-2"/>
+                        <label>2-3M</label>
+                        <input type="radio"  name="heightV" value="2-3"/>
+                        <label>Greater than 3M</label>
+                        <input type="radio"  name="heightV" value=">3"/>
+                    </form>
                     <p>Growth Rate</p><br/><br/>
-                    <p>Price</p><br/><br/>
+                    <form id="growthF">
+                        <label>Unchecked</label>
+                        <input type="radio" name="gRate" value="none" defaultChecked/>
+                        <label>Fast</label>
+                        <input type="radio" name="gRate" value="fastR"/>
+                        <label>Medium</label>
+                        <input type="radio" name="gRate" value="medR"/>
+                        <label>Slow</label>
+                        <input type="radio" name="gRate" value="slowR"/>
+                    </form><br/><br/>
                     <button type="button" onClick={this.check}>Submit</button><br/><br/>
                     <p id="result"></p><br/>
-                    <p id="image"></p>
                 </div>
             </div>
         )
     }
 
+    //check function for filter options
     check(){
+        results ="";
         for(var i = 0; i<trees.length;i++ )
         {
-            if((document.getElementById('categoryF').category.value===trees[i].category)&&
-            (document.getElementById('soilConF').soilCon.value===trees[i].soilCon)&&
-            (document.getElementById('sunConF').sunCon.value===trees[i].sunCon)&&
-            (document.getElementById('maintenF').mainten.value===trees[i].mainten))
+            //a really bad if statment chunk to display filtered options can work through later
+            if((document.getElementById('categoryF').category.value===trees[i].category||
+            document.getElementById('categoryF').category.value==="none")&&
+            (document.getElementById('soilConF').soilCon.value===(trees[i].soilCon)||
+            document.getElementById('soilConF').soilCon.value==="none")&&
+            (document.getElementById('sunConF').sunCon.value===trees[i].sunCon||
+            document.getElementById('sunConF').sunCon.value==="none")&&
+            (document.getElementById('maintenF').mainten.value===trees[i].mainten||
+            document.getElementById('maintenF').mainten.value==="none")&&
+            (document.getElementById('heightF').heightV.value===trees[i].heightV||
+            document.getElementById('heightF').heightV.value==="none")&&
+            (document.getElementById('growthF').gRate.value===trees[i].gRate||
+            document.getElementById('growthF').gRate.value==="none"))
             {
-                console.log("Result;"+trees[i].name);
-                document.getElementById('result').innerHTML="The Result was: "+trees[i].name;
-                document.getElementById('image').innerHTML="<img src='"+trees[i].pic+"'/>";
+                //make a long string holding search results
+                console.log("hit");
+                results+=trees[i].name+" <img src='"+trees[i].pic+"'/><br/>";
             }
         }
+        if(results!=="")
+        {
+            //change result paragraph to show the list of trees
+            document.getElementById('result').innerHTML="The Result was: "+results;
+        }
+        else
+        {
+            // this is within the for loop
+            document.getElementById('result').innerHTML="The Results show nothing";
+        }
     }
+
+    //blank search function
+    searchRes(){}
 }
--- src/components/treeSelect.js
+++ src/components/treeSelect.js
@@ -7,9 +7,11 @@ var oak = {name: "Oak",
     soilCon:"medDrain",
     sunCon:"sunny",
     mainten:"lowMain",
+    heightV:"<1",
+    gRate:"fastR",
     pic:oakpic};
 
-var spruce = {name: "Spruce", category:"hardwood", soilCon:"slow", sunCon:"shade", mainten:"medMain", pic:sprucepic};
-var jungle = {name: "Jungle", category:"hardwood", soilCon:"fast", sunCon:"medSun", mainten:"highMain", pic:junglepic};
+var spruce = {name: "Spruce", category:"hardwood", soilCon:"slow", sunCon:"shade", mainten:"medMain", heightV:"2-3", gRate:"medR", pic:sprucepic};
+var jungle = {name: "Jungle", category:"hardwood", soilCon:"fast", sunCon:"medSun", mainten:"highMain", heightV:">3", gRate:"slowR", pic:junglepic};
 var trees=[oak,spruce,jungle];
 export {trees};
