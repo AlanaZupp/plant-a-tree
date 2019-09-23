22b495ebdbc2263f36783e10702fe2943d914424
TimProctorAUT <42375425+TimProctorAUT@users.noreply.github.com>
2019-09-23 23:42:43 +1200

    Restructing Code to make it Webpage Responsive
    Started Restructuring and Rewriting some of the Main Components to Work with React-Bootstrapper and React-Router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,59 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import './App.css';
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 import HomePage from "./components/homepage.component";
 import SearchPage from "./components/searchpage.component";
 import BuyPage from "./components/buypage.component";
 
+
 class App extends Component {
   render(){
     return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/" className="navbar-brand">PlantATree</Link>
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/search" className="nav-link">Search</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/buy" className="nav-link">Buy</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        <Route path="/" exact component={HomePage}/>
-        <Route path="/search" component={SearchPage}/>
-        <Route path="/buy" component={BuyPage}/>
+      <div className="fullpage">      
+        <Router>
+          <div className="Nav">
+              <Navbar bg="dark" variant="dark" expand="md">
+                <Navbar.Brand><NavLink className="navbar-brand" to="/">LOGO</NavLink></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic=navbar-nav">
+                  <Nav variant="pills" className="mr-auto">
+                    <Nav.Item>
+                      <Nav.Link><NavLink className="nav-link" to="/" exact activeClassName="current">Home</NavLink></Nav.Link>   
+                    </Nav.Item>     
+
+                    <Nav.Item>
+                      <Nav.Link><NavLink className="nav-link" to="/search" activeClassName="current">Search</NavLink></Nav.Link>   
+                    </Nav.Item>    
+
+                    <Nav.Item>
+                      <Nav.Link><NavLink className="nav-link" to="/buy" activeClassName="current">Shopping Cart</NavLink></Nav.Link>   
+                    </Nav.Item>            
+                  </Nav>
+                </Navbar.Collapse>            
+              </Navbar>        
+          </div>
+
+          <div className="Body">
+            <Route path="/" exact component={HomePage}/>
+            <Route path="/search" component={SearchPage}/>
+            <Route path="/buy" component={BuyPage}/>  
+          </div>            
+        </Router>
+
+        <div className="Footer">
+          <div className="footer-copyright text-center py-3">
+              &copy; {new Date().getFullYear()} Copyright: Plant Wing Barn
+          </div>
         </div>
-      </Router>
+      </div>
     );
   }
 }
 
-export default App;
+  export default App;
--- src/components/homepage.component.js
+++ src/components/homepage.component.js
@@ -3,8 +3,11 @@ import React, {Component} from 'react';
 export default class HomePage extends Component{
     render(){
         return(
-            <div>
-                <p>Home Page</p>
+            <div className="fullpage">
+                <div>
+                    <p>Home Page</p>
+                </div>
+                
             </div>
         )
     }
