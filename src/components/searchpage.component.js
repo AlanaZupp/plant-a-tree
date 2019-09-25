262e690498c8c7ed4fde8be6103a3342757a019b
TimProctorAUT <42375425+TimProctorAUT@users.noreply.github.com>
2019-09-25 20:16:26 +1200

    Merging all CSS into One FIle

--- src/components/searchpage.component.js
+++ src/components/searchpage.component.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import SearchBox from "./SearchBox.component";
-import "./searchpage.css";
+
 //array of json tree objects
 import {trees} from "./tree.js";
 var results, count, resultsSec;
