3772be789955bba2f57a9112c39ce5d608f17c9f
Jordan Carter <alexkitteh@gmail.com>
2019-09-25 14:14:00 +1200

    Implement Tree Result, Add New Trees

--- src/components/tree.js
+++ src/components/tree.js
@@ -4,6 +4,10 @@ import sprucepic from "./treePics/spruce.png";
 import junglepic from "./treePics/jungle.png";
 import applepic from "./treePics/apple.png";
 import fernpic from "./treePics/fern.png";
+import acaciaPic from "./treePics/acacia.png";
+import birchPic from "./treePics/birch.png";
+import darkOakPic from "./treePics/dOak.png";
+import hedgePic from "./treePics/hedge.png";
 import nopic from "./treePics/NO_IMAGE.png";
 
 class Tree extends React.Component {
@@ -27,17 +31,50 @@ class Tree extends React.Component {
         var string = this.state.name + " <img src='"+this.state.pic+"'/>";
         return string;
     }
+    /*
+<div className="treecol">
+                <h3>BIRCH TREE</h3>
+                <p><b>Category:</b> Hardwood <br></br><b>Soil Drainage: </b>Medium<br></br><b>Sun Options:</b> Medium<br></br>
+                <b>Maintenance:</b> Low<br></br><b>Max height:</b>1-2M<br></br><b>Growth Rate:</b> Medium<br></br><b>Price: </b>$65</p>
+                <img src = {birch} alt=" " width="75" height="75"/>
+
+            </div>
+            */
+    info_string()
+    {
+        var string = "";
+        string += "<h3>" + this.state.name + "</h3>" +
+        "<p><b>Category: </b>" + this.state.category + "</br>" +
+        "<b>Soild Drainage:</b> " + this.state.soilCon + "</br>" +
+        "<b>Sun Options:</b> " + this.state.sunCon + "</br>" +
+        "<b>Maintenance:</b> " + this.state.mainten + "</br>" +
+        "<b>Max Height:</b> " + this.state.heightV + "</br>" +
+        "<b>Growth Rate:</b> " + this.state.gRate + "</br>" +
+        "<b>Price:</b> $" + this.state.price + "</br>" +
+        " <img src='"+this.state.pic+"'/>";
+        return string;
+    }
 }
 
-var oak = new Tree("Oak Tree", 150, "hardwood", "medDrain", "sunny", "lowMain", "<1", "fastR", oakpic);
+var oak = new Tree("Oak Tree", 150, "hardwood", "med", "sunny", "lowMain", "<1", "fastR", oakpic);
 
 var spruce = new Tree("Spruce Tree", 200, "hardwood", "slow", "shade", "medMain", "2-3", "medR", sprucepic);
 
 var jungle = new Tree("Jungle Tree", 250, "hardwood", "fast", "medSun", "highMain", ">3", "slowR", junglepic);
 
-var apple = new Tree("Apple Tree", 150, "fruit", "medDrain", "sunny", "medMain", "1-2", "medR", applepic);
+var apple = new Tree("Apple Tree", 150, "fruit", "med", "sunny", "medMain", "1-2", "medR", applepic);
 
-var fern = new Tree("Fern Tree", 100, "native", "medDrain", "shade", "lowMain", "1", "fastR", fernpic);
+var fern = new Tree("Fern Tree", 100, "native", "med", "shade", "lowMain", "1", "fastR", fernpic);
 
-var trees = [oak, spruce, jungle, apple,fern];
+var accacia = new Tree("Accacia Tree", 100, "palm", "fast", "sunny", "medMain", "2-3", "fastR", acaciaPic);
+
+var birch = new Tree("Birch Tree", 100, "hardwood", "med", "medSun", "highMain", "2-3", "slowR", birchPic);
+
+var darkOak = new Tree("Dark Oak Tree", 500, "hardwood", "med", "shade", "medMain", "2-3", "slow", darkOakPic);
+
+var hedge = new Tree("Hedge Tree", 34, "hedge", "fast", "shade", "lowMain", "1-2", "slowR", hedgePic);
+
+var gum = new Tree("Gum Tree", 100, "gum", "fast", "medSun", "medMain", ">3", "fastR", nopic);
+
+var trees = [oak, spruce, jungle, apple,fern, accacia, birch, darkOak, hedge, gum];
 export {trees}
